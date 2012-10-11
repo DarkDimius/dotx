988aeb669f588bb603e74290348f92890f085151
Eugene Burmako <xeno.by@gmail.com>
2012-10-11 11:35:49 +0200

    completed the installer

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+set -o errexit
 
 function install-into-home() {
-  if [[ -f "$HOME/$2" ]]; then mv "$HOME/$2" "$HOME/$2.bak"; fi
-  ln -s "$(dirname $0)/$1" "$HOME/$2"
+  if [[ -L "$HOME/$2" ]]; then
+    echo "$HOME/$2 already installed"
+    return 0;
+  fi
+  if [[ -f "$HOME/$2" ]]; then mv -v "$HOME/$2" "$HOME/$2.bak"; fi
+  ln -s -v "$(realpath $(dirname $0))/$1" "$HOME/$2"
 }
 
 install-into-home .gitconfig .gitconfig
@@ -10,3 +15,4 @@ install-into-home gitignore .gitignore
 install-into-home .scala_autorun .scala_autorun
 install-into-home .scala_history .scala_history
 install-into-home .zshrc .zshrc
+install-into-home .slate .slate
