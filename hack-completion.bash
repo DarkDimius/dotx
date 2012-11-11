b902537326e7abc707b63e9fb7f1077bd56a03b6
Eugene Burmako <xeno.by@gmail.com>
2012-11-11 16:48:23 +0100

    wip

--- hack-completion.bash
+++ hack-completion.bash
@@ -1,5 +1,4 @@
-_hack()
-{
+_hack() {
   local cur prev opts
   COMPREPLY=()
   cur="${COMP_WORDS[COMP_CWORD]}"
@@ -9,20 +8,50 @@ _hack()
 
   if [[ ${#COMP_WORDS[@]} == 2 ]]; then
     if [[ ${cur} == -* ]] ; then
-      opts="$(hack-homes --branches | while read line; do echo "-$line"; done)"
-      COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+      opts="$(hack-homes --short-branches | while read line; do echo "-$line"; done)"
+      COMPREPLY=( $(compgen -W "${opts} snippet macrosnippet" -- ${cur}) )
       return 0
     fi
 
-    opts="$(hack-homes --branches | while read line; do echo "$line"; done)"
-    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    opts="$(hack-homes --short-branches | while read line; do echo "$line"; done)"
+    COMPREPLY=( $(compgen -W "${opts} snippet macrosnippet" -- ${cur}) )
     return 0
   elif [[ ${#COMP_WORDS[@]} == 3 ]]; then
     if [[ ${prev} == +* ]] ; then
-      opts="$(hack-homes --branches | while read line; do echo "$line"; done)"
-      COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+      opts="$(hack-homes --short-branches | while read line; do echo "$line"; done)"
+      COMPREPLY=( $(compgen -W "${opts} snippet macrosnippet" -- ${cur}) )
       return 0
     fi
   fi
 }
 complete -F _hack hack
+
+_hack_home() {
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+  # unfortunately my bash fu is not enough to deduplicate this code
+
+  if [[ ${#COMP_WORDS[@]} == 2 ]]; then
+    opts="$(hack-homes --short-branches | while read line; do echo "$line"; done)"
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+  fi
+}
+complete -F _hack_home hack-home
+
+_hack_homes() {
+  local cur prev opts
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  opts="--dirs --short-branches --full-branches"
+
+  if [[ ${#COMP_WORDS[@]} == 2 ]]; then
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+  fi
+}
+complete -F _hack_homes hack-homes
