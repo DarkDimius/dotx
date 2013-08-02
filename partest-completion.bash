8b4e2bcfa87acea68fd108d70c93637c012c0b32
Eugene Burmako <xeno.by@gmail.com>
2013-08-02 09:05:31 +0200

    better test name mining in partest

--- partest-completion.bash
+++ partest-completion.bash
@@ -5,14 +5,14 @@ _partest() {
   prev="${COMP_WORDS[COMP_CWORD-1]}"
 
   if [[ ${cur} == -* ]]; then
-    COMPREPLY=( $(compgen -W "--debug --verbose --show-log --show-diff --update-check" -- ${cur}) )
+    COMPREPLY=( $(compgen -W "--debug --verbose --show-log --show-diff --update-check --all" -- ${cur}) )
     return 0
   else
     # TODO: deduplicate
-    if [[ ${cur} == */*/* ]]; then
-      filenames=$({ cd "$(scala-root)"; gfind "test/files" -path "test/${cur}*" ! -path "test/${cur}*/*" ! -path "*.flags" ! -path "*.check" ! -path "*.log" | sed "s/test\/\(.*\)/\1/"; })
+    if [[ ${cur} == */* ]]; then
+      filenames=$({ cd "$(scala-root)"; gfind "test/files" -path "test/files/${cur}*" ! -path "test/files/${cur}*/*" ! -path "*.flags" ! -path "*.check" ! -path "*.log" | sed "s/test\/files\/\(.*\)/\1/"; })
     else
-      filenames=$({ cd "$(scala-root)"; gfind "test/files" -path "test/${cur}*" ! -path "test/${cur}*/*" ! -path "*.flags" ! -path "*.check" ! -path "*.log" \( -type d -printf "%p/\n" , -type f -print \) | sed "s/test\/\(.*\)/\1/"; })
+      filenames=$({ cd "$(scala-root)"; gfind "test/files" -path "test/files/${cur}*" ! -path "test/files/${cur}*/*" ! -path "*.flags" ! -path "*.check" ! -path "*.log" \( -type d -printf "%p/\n" , -type f -print \) | sed "s/test\/files\/\(.*\)/\1/"; })
     fi
     COMPREPLY=( $(compgen -W "${filenames}" -- ${cur}) )
     [[ $COMPREPLY = */ ]] && compopt -o nospace
